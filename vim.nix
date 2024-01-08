1ac4d84ff290b192592fca1aae0c8e2369e97574
jossephus <yosephtuemay64@gmail.com>
2024-01-08 20:02:50 +0300

    change: version of vim to 8

--- vim.nix
+++ vim.nix
@@ -1,5 +1,16 @@
-{ pkgs, ...}: {
+{ pkgs, ...}: 
+let 
+  older_nixpkg = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "my-old-revision";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "f597e7e9fcf37d8ed14a12835ede0a7d362314bd";
+       }) {};
+in
+{
 	 programs.vim = {
+		package = older_nixpkg.vim; 
 		enable = true;
                 plugins = [
                   pkgs.vimPlugins.Vundle-vim
