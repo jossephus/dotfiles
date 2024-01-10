c32c316f2a3157780c174cb431ea94a700a840c5
jossephus <yosephtuemay64@gmail.com>
2024-01-10 18:22:56 +0300

    wip

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 					
 					    home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
-						home-manager.users.aldrich = import ./home.nix;
+						home-manager.users.aldrich = import ./home;
 					}
 				];
 			};
