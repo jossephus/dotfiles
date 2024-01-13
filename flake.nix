d3deb7aa29afe74c9da69a203560d6d29c5e7de7
jossephus <yosephtuemay64@gmail.com>
2024-01-13 21:41:43 +0300

    update home-manager to unstable

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 		alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
 
 		home-manager = {
-			url = "github:nix-community/home-manager/release-23.11";
+			url = "github:nix-community/home-manager";
 			# The `follows` keyword in inputs is used for inheritance.
 			# Here, `inputs.nixpkgs` of home-manager is kept consistent with
 			# the `inputs.nixpkgs` of the current flake,
