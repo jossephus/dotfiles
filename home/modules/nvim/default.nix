b80815b08906e1740ff45b182b61f7b8711dd95b
jossephus <yosephtuemay64@gmail.com>
2024-01-14 13:00:12 +0300

    wip

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -4,7 +4,7 @@
 		enable = true;
 	};
         # home.file.".vimrc".source = ./.vimrc;
-    home.file.".config/nvim" = ./config;
+    home.file.".config/nvim".source = ./config;
     #home.file.".config/nvim" = {
       #source = ./config;
       #recursive = true;
