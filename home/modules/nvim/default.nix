dfd1cf7231c9786c66b6a2cec79090f470f4640f
jossephus <yosephtuemay64@gmail.com>
2024-01-14 13:04:47 +0300

    wip

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -1,10 +1,17 @@
 { pkgs, ...}: 
 {
-	 programs.neovim = {
-		enable = true;
-	};
+    programs.neovim = {
+      enable = true;
+    };
+
+    xdg.configFile = {
+      "nvim" = {
+        source = ./config;
+        force = true;
+      };
+    };
         # home.file.".vimrc".source = ./.vimrc;
-    home.file.".config/nvim".source = ./config;
+    #home.file.".config/nvim".source = ./config;
     #home.file.".config/nvim" = {
       #source = ./config;
       #recursive = true;
