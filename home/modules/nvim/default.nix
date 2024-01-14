68cb30df2d4fad7002b485fb07ab5990c5fb1bd1
jossephus <yosephtuemay64@gmail.com>
2024-01-14 13:20:26 +0300

    wip

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -4,18 +4,21 @@
       enable = true;
     };
 
-    #xdg.configFile = {
-     # "nvim" = {
-     #   source = ./config;
-     #   force = true;
-     #   recursive = true;
-     # };
-    #};
+    xdg = {
+      enable = true;
+      configFile = {
+        "nvim" = {
+          source = ./config;
+          force = true;
+          recursive = true;
+        };
+      };
+    };
+
         # home.file.".vimrc".source = ./.vimrc;
     #home.file.".config/nvim".source = ./config;
     #home.file.".config/nvim" = {
       #source = ./config;
       #recursive = true;
     #};
-    xdg.configFile."nvim".source = ./config;
 }
