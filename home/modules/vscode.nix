feafe471e37b7a7067a02b6b65796e74d2cc399e
jossephus <yosephtuemay64@gmail.com>
2024-02-06 08:35:28 +0300

    feat: add vscode to configuration

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -3,15 +3,16 @@
     enable = true;
 
     extensions = with pkgs.vscode-extensions; [
-
-    ] ++ pkgs.vscode-utils.extensionFromVscodeMarketplace [
-      {
-         name = "remote-ssh-edit";
-          publisher = "ms-vscode-remote";
-          version = "0.47.2";
-      }
+          bbenoist.nix
 
     ];
+    #++ pkgs.vscode-utils.extensionFromVscodeMarketplace [
+      #{
+         #name = "remote-ssh-edit";
+         #publisher = "ms-vscode-remote";
+         #version = "0.47.2";
+      #}
+    #];
   };
 
 }
