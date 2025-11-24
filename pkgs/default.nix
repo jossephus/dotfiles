cf5bacc54138e04ca41606c5dacb9cd97b222ec7
jossephus <jossephustuemay@gmail.com>
2025-11-24 10:23:25 +0300

    feat: remove unused js files

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,23 +8,4 @@
     inherit wrapWine;
   };
   antigravity = pkgs.callPackage ./apps/antigravity.nix {};
-
-  vtsls = pkgs.buildNpmPackage rec {
-    name = "@vtsls/language-server";
-    version = "0.2.3";
-    src = pkgs.fetchurl {
-      url = "https://registry.npmjs.org/@vtsls/language-server/-/language-server-${version}.tgz";
-      hash = "sha256-VnMTKUqWpLxntV7RLU6Hsfet4XrdJhOrIjI3QcYpt6w=";
-    };
-    npmDepsHash = "sha256-N1K/kGlk2d8j1y6dcVyPuNDk9pYGslQFIVN1P3rLm+k=";
-    dontNpmBuild = true;
-    postPatch = ''
-      cp ${./package-lock.json} package-lock.json
-    '';
-    npmFlags = ["--legacy-peer-deps" "--loglevel=verbose"];
-    meta = {
-      mainProgram = "vtsls";
-    };
-  };
-  #memento = pkgs.callPackage ./apps/memento.nix { qtbase = { version = "5"; }; };
 }
