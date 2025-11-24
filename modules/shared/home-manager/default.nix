1bec40e9cfb65be157fc8884961e94bf84b31cd5
jossephus <jossephustuemay@gmail.com>
2025-11-24 09:57:37 +0300

    feat: refactor home manager packages to common place

--- modules/shared/home-manager/default.nix
+++ modules/shared/home-manager/default.nix
@@ -1,9 +1,18 @@
 # Toggle between two home-manager configuration approaches:
 # - nixified: Full Nix-based configuration
 # - sources: Static dotfiles with minimal Nix wrapping
-{inputs, ...}: {
+{inputs, pkgs, ...}: {
   imports = [
      #./nixified
     ./sources
+  ];
+
+  home.packages = with pkgs; [
+    delta
+    mergiraf
+    xh
+    biome
+    fd
+    findutils
   ];
 }
