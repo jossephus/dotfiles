0bd3daa80005efb83c3d01188bc3474a014f6b37
jossephus <jossephustuemay@gmail.com>
2026-08-10 08:37:49 +0300

    fix: avoid writing two json objects to config.json

--- hosts/darwin/system/nix.nix
+++ hosts/darwin/system/nix.nix
@@ -2,13 +2,14 @@
   nix.enable = false;
   nix.settings.experimental-features = "nix-command flakes";
 
-  determinateNix.customSettings.keep-outputs = true;
-
-  environment.etc."determinate/config.json".text = builtins.toJSON {
-    garbageCollector.strategy = "disabled";
-    builder = {
-      cpuCount = 1;
-      memoryBytes = 8589934592;
+  determinateNix = {
+    customSettings.keep-outputs = true;
+    determinateNixd = {
+      garbageCollector.strategy = "disabled";
+      builder = {
+        cpuCount = 1;
+        memoryBytes = 8589934592;
+      };
     };
   };
 }
