1051abd1dc94a4ec32b5f9c0510e6c27565325d0
jossephus <jossephustuemay@gmail.com>
2026-08-22 08:50:57 +0300

    feat: remove daily updates

--- users/jossephus/ai.nix
+++ users/jossephus/ai.nix
@@ -1,9 +1,14 @@
-{ pkgs, inputs, ... }: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
     agent-browser
     herdr
     pi
     skills
     terminal-use
+    fx
   ];
 }
